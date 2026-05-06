da2476549391623fbb9eb77f7a6fc5d5dc4121aa
narasimhauppala <uppalanarasimha79@gmail.com>
2026-05-06 19:20:52 +0530

    test: use pytest functional contract

--- test-version.js
+++
@@ -1,8 +0,0 @@
-const lodash = require("lodash/package.json");
-
-if (lodash.version !== "4.17.20") {
-  console.error(`Functional contract changed: expected lodash 4.17.20, got ${lodash.version}`);
-  process.exit(1);
-}
-
-console.log(`Functional contract OK for lodash ${lodash.version}`);
